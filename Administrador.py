48c40389c0a98aea2bc35bb2f0ae3efec1d12413
SimonDiceXx <bryan000123456@gmail.com>
2026-06-02 18:25:03 -0500

    Update Administrador.py
    Aqui creamos una funcion para gestionar los usuarios , otra para gestionar procesos administratibos  y otra para generar reportes

--- Administrador.py
+++ Administrador.py
@@ -13,3 +13,23 @@ class Administrador(Usuario):
     @cargo.setter
     def cargo(self, valor):
         self.__cargo = valor
+#gestiona usuario del sistema puede activarlos o desactivarlos
+    def gestionar_usuario(self, accion, usuario, **kwargs):
+        if accion == "activar":
+            usuario.estado = True
+            print("Usuario " + usuario.nombres + " activado")
+        elif accion == "desactivar":
+            usuario.estado = False
+            print("Usuario " + usuario.nombres + " desactivado")
+        else:
+            print("Accion no reconocida")
+        if "motivo" in kwargs:
+            print("Motivo: " + kwargs["motivo"])
+        if "fecha" in kwargs:
+            print("Fecha del cambio: " + kwargs["fecha"])
+#gestiona procesos administrativos
+    def gestionar_procesos(self, proceso):
+        print("Proceso ejecutado: " + proceso)
+#genera reportes
+    def gestionar_reportes(self):
+        print("Reporte generado por: " + self.nombres + " " + self.apellidos + " cargo: " + self.__cargo)
