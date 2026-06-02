bafc4d7ee15e190471e1949b813b046c6745434e
CLMaykel <cmaikel221@gmail.com>
2026-06-02 15:25:21 -0500

    Update Docente.py
    Usando kwargs se registran las notas de un estudiante
    Sobrecarga simulada con kwargs

--- Docente.py
+++ Docente.py
@@ -21,4 +21,31 @@ class Docente(Usuario):                           #se inicializan los atributos 
     @especialidad.setter
     def especialidad(self, valor):
         self.__especialidad = valor
-      
+#se registra las notas de un estudiante usando kwargs
+    def registrar_notas(self, id_calificacion, id_estudiante, parcial1, parcial2, **kwargs):
+        #se valida que las notas estén entre 0 y 10
+        if parcial1 < 0 or parcial1 > 10 or parcial2 < 0 or parcial2 > 10:
+            print("Las notas deben estar entre 0 y 10")
+            return
+        nota_final = round((parcial1 + parcial2) / 2, 2)
+    #se crea un diccionario con la información de la calificación
+        nota = {
+            "id_calificacion": id_calificacion,
+            "id_estudiante": id_estudiante,
+            "parcial1": parcial1,
+            "parcial2": parcial2,
+            "nota_final": nota_final
+        }
+        if "observacion" in kwargs:
+            nota["observacion"] = kwargs["observacion"]
+        if "fecha" in kwargs:
+            nota["fecha"] = kwargs["fecha"]
+        self.__notas_registradas.append(nota)
+        print("Nota registrada para el estudiante " + str(id_estudiante) + " nota final: " + str(nota_final))
+#sobrecarga simulada con args
+    def registrar_asistencia(self, fecha, estado, *args):
+        for id_estudiante in args: #recorre cada estudiante enviado en args
+            print("Asistencia registrada para el estudiante " + str(id_estudiante) + " el " + fecha + " estado: " + estado)
+
+    def consultar_estudiantes(self, curso):
+        return curso.lista_estudiantes
