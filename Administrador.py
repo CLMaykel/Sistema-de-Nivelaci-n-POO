0268f89e8de071981519503a38fc9d2dacc4c12f
SimonDiceXx <bryan000123456@gmail.com>
2026-06-02 18:20:56 -0500

    Update Administrador.py
    Aqui creamos la clase Administrador con sus respectivos atributos y propiedades

--- Administrador.py
+++ Administrador.py
@@ -1,1 +1,15 @@
+from usuario import Usuario
+class Administrador(Usuario):
 
+    def __init__(self, id_usuario, cedula, nombres, apellidos, correo, contraseña, telefono, id_administrador, cargo):
+        super().__init__(id_usuario, cedula, nombres, apellidos, correo, contraseña, telefono)
+        self.__id_administrador = id_administrador
+        self.__cargo = cargo
+
+    @property
+    def cargo(self):
+        return self.__cargo
+
+    @cargo.setter
+    def cargo(self, valor):
+        self.__cargo = valor
