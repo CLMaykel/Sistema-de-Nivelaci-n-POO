32404421ff1590716c4f27a6c7c04a0f61dbd488
Jaimeq2007 <jostinquijijesuarez@gmail.com>
2026-06-16 16:36:06 -0500

    comprobar y arreglar si hay errores

---
+++ detalle_calificacion.py
@@ -0,0 +1,45 @@
+class DetalleCalificacion:
+
+    def __init__(self, id_detalle, tipo_evaluacion, descripcion, puntaje_obtenido, puntaje_total, fecha_evaluacion):
+        self.__id_detalle = id_detalle
+        self.__tipo_evaluacion = tipo_evaluacion
+        self.__descripcion = descripcion
+        self.__puntaje_obtenido = puntaje_obtenido
+        self.__puntaje_total = puntaje_total
+        self.__fecha_evaluacion = fecha_evaluacion
+#uso de propiedades para acceder a los atributos privados
+    @property
+    def tipo_evaluacion(self):
+        return self.__tipo_evaluacion
+
+    @property
+    def puntaje_obtenido(self):
+        return self.__puntaje_obtenido
+
+    @property
+    def puntaje_total(self):
+        return self.__puntaje_total
+#se calcula el porcentaje de la evaluacion con base al puntaje obtenido y el puntaje total
+    @property
+    def porcentaje(self):
+        if self.__puntaje_total == 0:
+            return 0
+        resultado = (self.__puntaje_obtenido / self.__puntaje_total) * 100
+        return round(resultado, 2)
+
+    @puntaje_obtenido.setter
+    def puntaje_obtenido(self, valor):
+        self.__puntaje_obtenido = valor
+
+    def registrar_detalle(self):
+        print("Detalle registrado: " + self.__tipo_evaluacion + " puntaje: " + str(self.__puntaje_obtenido) + "/" + str(self.__puntaje_total))
+#editamos el puntaje obtenido en la evaluacion
+    def editar_detalle(self, nuevo_puntaje):
+        self.__puntaje_obtenido = nuevo_puntaje
+        print("Puntaje actualizado a " + str(nuevo_puntaje))
+
+    def eliminar_detalle(self):
+        print("Detalle eliminado")
+#se muetra un resumen del detalle de calificacion 
+    def mostrar_info(self):
+        print("Evaluacion: " + self.__tipo_evaluacion + " " + str(self.__puntaje_obtenido) + "/" + str(self.__puntaje_total) + " porcentaje: " + str(self.porcentaje) + "%")
