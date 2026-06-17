a18abedf224f6b389450cb3e1e00922472cefb5c
pablozuve2060 <zunigapablo2901@gmail.com>
2026-06-16 20:03:50 -0500

    Update FabricaUsuario_Cambio1.py

--- FabricaUsuario_Cambio1.py
+++ FabricaUsuario_Cambio1.py
@@ -1,16 +1,12 @@
-# Cambio 1: Fabrica de usuarios para el sistema de nivelación
-# Este archivo es un módulo adicional y no modifica los archivos existentes.
-# una clase FabricaUsuario que crea instancias de Estudiante, Docente o Administrador.
-
-from usuario import Usuario
+#importamos las clases de los diferentes tipos de usuarios
 from docente import Docente
 from estudiante import Estudiante
 from admin import Administrador
 
-
+#hacemos uso del patrón Factory Method para crear una fábrica de usuarios
 class FabricaUsuario:
-    """Fábrica para crear usuarios del sistema según su tipo."""
-
+    
+    #método crear_usuario que recibe el tipo de usuario y los argumentos necesarios para crear una instancia del mismo 
     def crear_usuario(self, tipo_usuario, *args, **kwargs):
         if tipo_usuario == "Estudiante":
             return Estudiante(*args, **kwargs)
@@ -19,49 +15,5 @@ class FabricaUsuario:
         elif tipo_usuario == "Administrador" or tipo_usuario == "Admin":
             return Administrador(*args, **kwargs)
         else:
+            #si el tipo de usuario ingresado no conduerda con ninguno de los casos anteriores, se lanza una excepción 
             raise ValueError(f"Tipo de usuario no válido: {tipo_usuario}")
-
-
-# fabrica = FabricaUsuario()
-# estudiante = fabrica.crear_usuario(
-#     "Estudiante",
-#     2,
-#     "1300002222",
-#     "Maykel",
-#     "Castro",
-#     "mcastro@uleam.edu.ec",
-#     "est123",
-#     "0997654321",
-#     "Cedula",
-#     "1300002222",
-#     "2005-03-15"
-# )
-#
-# docente = fabrica.crear_usuario(
-#     "Docente",
-#     1,
-#     "1300001111",
-#     "Valentin",
-#     "Perez",
-#     "perez123@uleam.edu.ec",
-#     "doc123",
-#     "0991234567",
-#     "Magister en Software",
-#     "Programacion OO"
-# )
-#
-# admin = fabrica.crear_usuario(
-#     "Administrador",
-#     4,
-#     "1300004444",
-#     "Carlos",
-#     "Ortiz",
-#     "cortiz@uleam.edu.ec",
-#     "adm123",
-#     "0993456789",
-#     1,
-#     "Director de Nivelacion"
-# )
-#
-# for usuario in [estudiante, docente, admin]:
-#     usuario.mostrar_info()
