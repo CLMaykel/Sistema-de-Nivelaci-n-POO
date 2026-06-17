ad27e456e3872e0bb8d6c6e2859c12050ff365dc
pablozuve2060 <zunigapablo2901@gmail.com>
2026-06-17 07:27:43 -0500

    Enhance IExportable and implementations with comments
    Added comments to clarify the purpose of IExportable and its implementations.

--- modelos/iexportable.py
+++ modelos/iexportable.py
@@ -1,20 +1,20 @@
 from abc import ABC, abstractmethod
-
+#creamos la interfaz IExportable para exportar datos en diferentes formatos
 class IExportable(ABC):
-
+    #metodo con el que se exportaran los datos, cada clase que implemente esta interfaz debe definir este metodo
     @abstractmethod
     def exportar(self, datos):
         pass
 
-
+#clase ExportarExcel que exporta datos a formato Excel
 class ExportarExcel(IExportable):
 
     def exportar(self, datos):
         print("Exportando a Excel:", datos)
 
-
+#clase ExportarPDF que exporta datos a formato PDF
 class ExportarPDF(IExportable):
-
+    
     def exportar(self, datos):
         print("Exportando a PDF:", datos)
 
