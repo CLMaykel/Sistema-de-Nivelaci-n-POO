9bcd4412e000aba732aa66461730dd1380d94716
SimonDiceXx <bryan000123456@gmail.com>
2026-07-07 17:29:16 -0500

    Implement conectar method for SQL Server connection
    Add a method to connect to SQL Server using pyodbc.

--- servicios/BaseD.py
+++ servicios/BaseD.py
@@ -8,3 +8,21 @@ class ConexionDB:
         
         self.conn = None
         self.cursor = None
+        
+    def conectar(self):
+        try:
+
+            conexion_str = (
+                f"DRIVER={self.driver};"
+                f"SERVER={self.server};"
+                f"DATABASE={self.database};"
+                "Trusted_Connection=yes;"
+                "Encrypt=no;"
+            )
+            
+            self.conn = pyodbc.connect(conexion_str)
+            self.cursor = self.conn.cursor()
+            
+        except pyodbc.Error as e:
+            print(f"Error crítico al conectar a SQL Server: {e}")
+            raise e
